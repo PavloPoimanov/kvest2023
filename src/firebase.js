9c6d26d53150fa20ecaff4a2f9120d2ba0141f01
Pavlo <pauluxxx746@gmail.com>
2023-09-23 00:16:44 +0300

    add fire base conf

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCYTqeEQ4PXinvsjgXMu3LRfdBbJOFz9lU",
+    authDomain: "kvest2023-25bd1.firebaseapp.com",
+    projectId: "kvest2023-25bd1",
+    storageBucket: "kvest2023-25bd1.appspot.com",
+    messagingSenderId: "493447587998",
+    appId: "1:493447587998:web:64adcc7763c0bc87a8338f",
+    measurementId: "G-3J55FHEB6B",
+    databaseURL: "https://kvest2023-25bd1-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const auth = getAuth(app);
