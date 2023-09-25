607b47ab19e8c06a7b1f49005f0d9a4d53542cd8
Pavlo <pauluxxx746@gmail.com>
2023-09-25 11:58:37 +0300

    navbar and header, add headless ui

---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import {Link} from "react-router-dom";
+
+export const Logo = () => {
+    return <Link to="/" className="text-2xl text-gray-300 hover:bg-gray-700 font-semibold hover:text-white">Kvest
+        2023</Link>
+}
--- src/containers/layout/Header.js
+++ src/containers/layout/Header.js
@@ -1,32 +1,19 @@
-import {Link} from 'react-router-dom';
 import React from 'react';
+import {Navbar} from "../../components/Navbar";
+import {useLocation} from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header className="bg-white py-4 shadow">
-            <div className="container mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-semibold text-blue-gray">Kvest 2023</h1>
-                <nav>
-                    <ul className="flex space-x-4">
-                        <li>
-                            <Link className="text-blue-gray hover:text-light-blue-gray transition duration-300" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/names' className="text-blue-gray hover:text-light-blue-gray transition duration-300">
-                                    Names
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/testTheme' className="text-blue-gray hover:text-light-blue-gray transition duration-300">
-                                    Theme check
-                            </Link>
+    // Get the current location using React Router's useLocation hook
+    const location = useLocation();
 
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+    // Define the navigation links with their "isActive" property
+    const navigationLinks = [
+        { name: "Names", to: "/names", current: location.pathname === "/names" },
+        { name: "Places", to: "/places", current: location.pathname === "/places" },
+        { name: "Numbers", to: "/numbers", current: location.pathname === "/numbers" },
+        { name: "Quotes", to: "/quotes", current: location.pathname === "/quotes" }
+    ];
+    return (
+        <Navbar navigation={navigationLinks}/>
     );
 };
