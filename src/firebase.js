6c66b7be252a41d01bca8cee7f5b6e5be99bcd43
Pavlo <pauluxxx746@gmail.com>
2023-10-13 00:14:37 +0300

    secrects gone away

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,14 @@ import {connectAuthEmulator, getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCYTqeEQ4PXinvsjgXMu3LRfdBbJOFz9lU",
-    authDomain: "kvest2023-25bd1.firebaseapp.com",
-    projectId: "kvest2023-25bd1",
-    storageBucket: "kvest2023-25bd1.appspot.com",
-    messagingSenderId: "493447587998",
-    appId: "1:493447587998:web:64adcc7763c0bc87a8338f",
-    measurementId: "G-3J55FHEB6B",
-    databaseURL: "https://kvest2023-25bd1-default-rtdb.firebaseio.com/",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
+    databaseURL: process.env.REACT_APP_databaseURL,
 };
 
 // Initialize Firebase
